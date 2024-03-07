95dff8c617dedb5b1fc661f834cf8e4bb25820d1
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-03-07 21:43:13 +0000

    worked on profile links for 'posts' and 'liked photos'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+
+
   def show
     @user = User.find_by!(username: params.fetch(:username))
   end
+  
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
+
+  get ":username/liked" => "users#liked", as: :liked
   
-  get "/:username" => "users#show", as: :user
+  get ":username" => "users#show", as: :user
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
