bbc63144f4aa81dc0715ddb54670a40f1bc5e2fd
crystal tliourane <129141063+ctlio@users.noreply.github.com>
2024-03-11 04:46:25 +0000

    added tabbed interface for feed/following/followers, and added shared layout list.html.erb for following/followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,16 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def following
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end
